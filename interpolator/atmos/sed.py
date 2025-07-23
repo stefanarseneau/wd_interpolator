487b7a6872016e46975e2957a848ce332bb10f92
Stefan Arseneau <arseneausm@gmail.com>
2025-07-23 10:34:13 -0400

    added more J-PLUS filters

--- interpolator/atmos/sed.py
+++ interpolator/atmos/sed.py
@@ -51,31 +51,42 @@ def load_from_path(path, filterkws):
 def get_default_filters(filterkws = {}):
     dirname = os.path.dirname(os.path.abspath(__file__)) 
     return {
+        # Gaia Photometry
         'Gaia_G' : load_from_path(os.path.join(dirname, 'bandpasses/Gaia_G.npy'), filterkws),
         'Gaia_BP' : load_from_path(os.path.join(dirname, 'bandpasses/Gaia_BP.npy'), filterkws),
         'Gaia_RP' : load_from_path(os.path.join(dirname, 'bandpasses/Gaia_RP.npy'), filterkws),
+        # SDSS Photometry
         'SDSS_u' : load_from_path(os.path.join(dirname, 'bandpasses/SDSS_u.npy'), filterkws),
         'SDSS_g' : load_from_path(os.path.join(dirname, 'bandpasses/SDSS_g.npy'), filterkws),
         'SDSS_r' : load_from_path(os.path.join(dirname, 'bandpasses/SDSS_r.npy'), filterkws),
         'SDSS_i' : load_from_path(os.path.join(dirname, 'bandpasses/SDSS_i.npy'), filterkws),
         'SDSS_z' : load_from_path(os.path.join(dirname, 'bandpasses/SDSS_z.npy'), filterkws),
+        # PanSTARRS DR1
         'PS1_g' : load_from_path(os.path.join(dirname, 'bandpasses/PS1_g.npy'), filterkws),
         'PS1_r' : load_from_path(os.path.join(dirname, 'bandpasses/PS1_r.npy'), filterkws),
         'PS1_i' : load_from_path(os.path.join(dirname, 'bandpasses/PS1_i.npy'), filterkws),
         'PS1_z' : load_from_path(os.path.join(dirname, 'bandpasses/PS1_z.npy'), filterkws),
         'PS1_y' : load_from_path(os.path.join(dirname, 'bandpasses/PS1_y.npy'), filterkws),
-        'JPLUS_J0395' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0395.npy'), filterkws),
-        'JPLUS_F0395' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0395.npy'), filterkws),
-        'JPLUS_F0861' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0861.npy'), filterkws),
-        'JPLUS_J0430' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0430.npy'), filterkws),
-        'JPLUS_F0430' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0430.npy'), filterkws),
-        'JPLUS_J0515' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0515.npy'), filterkws),
+        # J-PLUS F Filters
         'JPLUS_F0378' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0378.npy'), filterkws),
-        'JPLUS_J0861' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0861.npy'), filterkws),
-        'JPLUS_F0515' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0515.npy'), filterkws),
-        'JPLUS_J0378' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0378.npy'), filterkws),
+        'JPLUS_F0395' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0395.npy'), filterkws),
         'JPLUS_F0410' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0410.npy'), filterkws),
-        'JPLUS_J0660' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0660.npy'), filterkws),
+        'JPLUS_F0430' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0430.npy'), filterkws),   
+        'JPLUS_F0515' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0515.npy'), filterkws),
+        'JPLUS_F0660' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0660.npy'), filterkws),
+        'JPLUS_F0861' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0861.npy'), filterkws),
+        # J-PLUS J Filters
+        'JPLUS_J0378' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0378.npy'), filterkws),
+        'JPLUS_J0395' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0395.npy'), filterkws),
         'JPLUS_J0410' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0410.npy'), filterkws),
-        'JPLUS_F0660' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_F0660.npy'), filterkws)
+        'JPLUS_J0430' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0430.npy'), filterkws),
+        'JPLUS_J0515' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0515.npy'), filterkws),
+        'JPLUS_J0660' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0660.npy'), filterkws),
+        'JPLUS_J0861' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_J0861.npy'), filterkws),
+        # J-PLUS SDSS
+        'JPLUS_uJAVA' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_uJAVA.npy'), filterkws),
+        'JPLUS_gSDSS' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_gSDSS.npy'), filterkws),
+        'JPLUS_rSDSS' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_rSDSS.npy'), filterkws),
+        'JPLUS_iSDSS' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_iSDSS.npy'), filterkws),
+        'JPLUS_zSDSS' : load_from_path(os.path.join(dirname, 'bandpasses/JPLUS_zSDSS.npy'), filterkws),
     }
